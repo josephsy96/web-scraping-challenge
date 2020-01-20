7bd6230806f79c4f600730f9e138fe2c2f053cbc
josephsy96 <josephsy96@hotmail.com>
2020-01-20 14:37:46 -0600

    update Mars Facts

--- scrape_mars.py
+++ scrape_mars.py
@@ -75,5 +75,84 @@ def scrape():
 
     only_weather = [m for m in mars_weather if "InSight" in m]
     fwt = only_weather[0]
-    
+
 #=====================================================================================
+
+#=====================================================================================
+    #Master Lists
+    title_list = []
+    url_list = []
+
+    #Cerberus Hemi 
+    c_url = "https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced"
+
+    c_html = requests.get(c_url)
+
+    #Hemi BeautifulSoup
+    c_soup = bs(c_html.text,'html.parser')
+
+    #Pull title and image
+    cerb_site = c_soup.find_all('div',{'class':'container'})
+
+    for ct in cerb_site:
+        cerb_img_url = ct.find('img',{'class':'wide-image'})['src']
+        cerb_title = ct.find('h2',{'class':'title'})
+        
+        title_list.append(cerb_title.text)
+        url_list.append(c_url + cerb_img_url)
+    
+    #Schiaparelli URL
+    s_url = "https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced"
+
+    s_html = requests.get(s_url)
+
+    #Schiaparelli BeautifulSoup
+    s_soup = bs(s_html.text,'html.parser')
+
+    #Pull title and image
+    schi_site = s_soup.find_all('div',{'class':'container'})
+
+    for st in schi_site:
+        schi_image_url = st.find('img',{'class':'wide-image'})['src']
+        schi_title = st.find('h2',{'class':'title'})
+        
+        url_list.append(s_url + schi_image_url)
+        title_list.append(schi_title.text)
+        
+    #Syrtis URL
+    sy_url = "https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced"
+
+    sy_html = requests.get(sy_url)
+
+    #Syrtis BeautifulSoup
+    sy_soup = bs(sy_html.text, 'html.parser')
+
+    #Pull title and image
+    sy_site = sy_soup.find_all('div',{'class':'container'})
+
+    for syt in sy_site:
+        sy_img_url = syt.find('img',{'class':'wide-image'})['src']
+        sy_title = syt.find('h2',{'class':'title'}).text
+        
+        title_list.append(sy_title)
+        url_list.append(sy_url + sy_img_url)
+
+    #Valles URL
+    v_url = "https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced"
+
+    v_html = requests.get(v_url)
+
+    #Valles BeautifulSoup
+    v_soup = bs(v_html.text, 'html.parser')
+
+    v_site = v_soup.find_all('div', {'class':'container'})
+
+    for vt in v_site:
+        v_img_url = vt.find('img', {'class':'wide-image'})['src']
+        v_title = vt.find('h2',{'class':'title'}).text
+        
+        url_list.append(v_url + v_img_url)
+        title_list.append(v_title)
+        
+    #Create Dictionary
+    hemi_dict = {'title': title_list,'hemisphere': url_list}
