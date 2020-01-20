fc0db32dc8e128fea20d001c121c54fc033c5e15
josephsy96 <josephsy96@hotmail.com>
2020-01-20 14:21:09 -0600

    update mission to mars

---
+++ scrape_mars.py
@@ -0,0 +1,9 @@
+#Import Dependencies
+from splinter import Browser
+from splinter.exceptions import ElementDoesNotExist
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+
+def scrape():
+    
